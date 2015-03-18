95361290fd62158626b22f5872e8f5a41cb43ee3
holyshared <holy.shared.design@gmail.com>
2015-03-18 07:45:26 +0000

    Add chmod

--- src/TemporaryDirectory.php
+++ src/TemporaryDirectory.php
@@ -2,31 +2,59 @@
 
 namespace holyshared\peridot\temporary;
 
+use \SplFileInfo;
+
 
 class TemporaryDirectory extends TemporaryNode
 {
 
-    private $path;
+    private $node;
+
 
     public function __construct($name, $mode = 0755)
     {
-        $this->path = sys_get_temp_dir() . '/' . $name;
-        mkdir($this->path, $mode, true); //recursive!!
+        $path = sys_get_temp_dir() . '/' . $name;
+        mkdir($path, $mode, true); //recursive!!
+
+        $this->node = new SplFileInfo($path);
     }
 
     public function getPath()
     {
-        return $this->path;
+        return $this->node->getPathname();
     }
 
     public function exists()
     {
-        return file_exists($this->path);
+        return file_exists($this->getPath());
+    }
+
+    public function chmod($mode)
+    {
+        chmod($this->getPath(), $mode);
+    }
+
+    public function getPermission()
+    {
+        $value = $this->node->getPerms();
+        $value = substr(sprintf('%o', $value), -4); //Example: 100444
+
+        return intval($value, 8);
+    }
+
+    public function isReadable()
+    {
+        return $this->node->isReadable();
+    }
+
+    public function isWritable()
+    {
+        return $this->node->isWritable();
     }
 
     public function remove()
     {
-        rmdir($this->path);
+        rmdir($this->getPath());
     }
 
     public function __destruct()
