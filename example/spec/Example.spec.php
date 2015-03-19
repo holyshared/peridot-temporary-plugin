be81077dccb42279312af6f51ec36c21734168a1
holyshared <holy.shared.design@gmail.com>
2015-03-19 03:12:11 +0000

    Add example spec

--- example/spec/Example.spec.php
+++ example/spec/Example.spec.php
@@ -2,15 +2,25 @@
 
 describe('Example', function() {
     describe('#makeDirectory', function() {
-        beforeEach(function() {
-            $this->temp = $this->makeDirectory();
+        context('when not specify a permission', function() {
+            beforeEach(function() {
+                $this->temp = $this->makeDirectory();
+            });
+            it('create temporary directory', function() {
+                expect($this->temp->exists())->toBeTrue();
+            });
         });
-        it('create temporary directory', function() {
-            expect($this->temp->exists())->toBeTrue();
+        context('when there is a permission of the specified', function() {
+            beforeEach(function() {
+                $this->temp = $this->makeDirectory(0644);
+            });
+            it('create temporary directory', function() {
+                expect($this->temp->exists())->toBeTrue();
+            });
         });
     });
     describe('#makeFile', function() {
-        context('When not specify a directory', function() {
+        context('when not specify a permission', function() {
             beforeEach(function() {
                 $this->temp = $this->makeFile();
             });
@@ -18,9 +28,9 @@ describe('Example', function() {
                 expect($this->temp->exists())->toBeTrue();
             });
         });
-        context('when there is a directory of the specified', function() {
+        context('when there is a permission of the specified', function() {
             beforeEach(function() {
-                $this->temp = $this->makeFile('foo');
+                $this->temp = $this->makeFile(0644);
             });
             it('create temporary file', function() {
                 expect($this->temp->exists())->toBeTrue();
